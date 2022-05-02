2510058d11e47b4faf10da31d2b10601820fb9bd
Tom Elliott <tom.elliott@alphasights.com>
2022-05-02 16:24:47 +0100

    Integration test cleans up after itself

--- src/test/java/com/elliott/rpg/controller/PlayerControllerIntegrationTest.java
+++ src/test/java/com/elliott/rpg/controller/PlayerControllerIntegrationTest.java
@@ -2,7 +2,7 @@ package com.elliott.rpg.controller;
 
 import com.elliott.rpg.core.Game;
 import com.elliott.rpg.domain.Player;
-import com.elliott.rpg.domain.inventory.Inventory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,17 +18,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class PlayerControllerIntegrationTest {
 
+    private static final String PLAYER_NAME = "Test Player";
+
     @Autowired
     private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        Game.getGame().setPlayer(new Player(PLAYER_NAME));
+        Game.getGame().getPlayer().getInventory().empty();
+    }
 
     @Test
     public void testGetPlayer() {
 
-        Game.getGame().setPlayer(new Player("Test Player"));
-
         try {
             this.mockMvc.perform(MockMvcRequestBuilders.get("/player"))
-                    .andDo(print())
                     .andExpect(content().string(containsString(
                             "{\"name\":\"Test Player\",\"inventory\":{\"items\":[]},\"id\":1}")))
                     .andExpect(status().isOk());
@@ -41,11 +45,8 @@ public class PlayerControllerIntegrationTest {
     @Test
     public void testGetPlayerInventory() {
 
-        Game.getGame().setPlayer(new Player("Test Player"));
-
         try {
             this.mockMvc.perform(MockMvcRequestBuilders.get("/player/inventory"))
-                    .andDo(print())
                     .andExpect(content().string(containsString("{\"items\":[]}")))
                     .andExpect(status().isOk());
         } catch (Exception e) {
@@ -56,21 +57,15 @@ public class PlayerControllerIntegrationTest {
     @Test
     public void testAddItemToPlayerInventory() {
 
-        Game.getGame().setPlayer(new Player("Test Player"));
-
         String item = "{\"attackSpeed\":1.2,\"attackDamage\":10,\"itemRarity\":\"NORMAL\"}";
         try {
             this.mockMvc.perform(MockMvcRequestBuilders.put("/player/inventory")
                             .queryParam("itemJson", item))
-                    .andDo(print())
                     .andExpect(content().string(containsString("{\"items\":[{\"attackSpeed\":1.2,\"attackDamage\":10," +
                             "\"itemRarity\":\"NORMAL\",\"level\":0,\"damagePerSecond\":12.0,\"itemType\":\"WEAPON\"}]}")))
                     .andExpect(status().isOk());
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        //reset
-        Game.getGame().getPlayer().getInventory().empty();
     }
 }
